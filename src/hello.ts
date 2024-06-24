b0f10d399ecd3644955169766bb7d729c1b5a318
mywyau <47248104+mywyau@users.noreply.github.com>
2024-06-24 22:25:31 +0100

    add tests for tomy

--- src/hello.ts
+++ src/hello.ts
@@ -1,5 +1,11 @@
 export function greet(): string {
     return "Hello, World!";
   }
+
+export function greet_tomy(): string {
+  return "Hello, Tomy!";
+}
   
-  console.log(greet());
+console.log(greet());  
+
+console.log(greet_tomy());
--- tests/hello.test.ts
+++ tests/hello.test.ts
@@ -1,5 +1,9 @@
-import { greet } from "../src/hello";
+import { greet, greet_tomy } from "../src/hello";
 
 test('greet returns "Hello, World!"', () => {
     expect(greet()).toBe("Hello, World!");
+});
+
+test('greet_tomy returns "Hello, Tomy!"', () => {
+    expect(greet_tomy()).toBe("Hello, Tomy!");
 });
