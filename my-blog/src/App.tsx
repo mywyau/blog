fb1bd594d1ccf7bae80e504338c49b6789024a4a
Michael Yau <47248104+mywyau@users.noreply.github.com>
2024-07-21 02:44:12 +0100

    sections add more sections (#19)

--- my-blog/src/App.tsx
+++ my-blog/src/App.tsx
@@ -5,6 +5,8 @@ import LandingPage from './views/components/pages/LandingPage';
 import BlogPost from './views/components/blog/BlogPost';
 import About from './views/components/pages/About';
 import Contacts from './views/components/pages/Contacts';
+import Skills from './views/components/pages/Skills';
+import WorkLog from './views/components/pages/Irl';
 
 
 const App: React.FC = () => {
@@ -14,6 +16,8 @@ const App: React.FC = () => {
       <Route path="/post/:id" element={<BlogPost />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contacts />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/work-log" element={<WorkLog />} />
     </Routes>
   );
 };
