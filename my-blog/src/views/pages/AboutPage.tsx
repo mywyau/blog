300241ef864c2402acb960b08bbcb2d03789dc7a
mywyau <47248104+mywyau@users.noreply.github.com>
2024-08-22 19:14:06 +0100

    update ids

--- my-blog/src/views/pages/AboutPage.tsx
+++ my-blog/src/views/pages/AboutPage.tsx
@@ -7,9 +7,9 @@ const About: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen font-nunito bg-gray-100">
       <Navbar />
-      <div className="flex-grow container mx-auto p-5">
-        <div className="flex justify-center">
-          <h1 className="text-6xl font-bold bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent pt-6">
+      <div className="p-4 pb-6 container mx-auto">
+        <div className="flex justify-right">
+          <h1 id="about" className="text-6xl font-bold bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent pt-6">
             {messages.about.title}
           </h1>
         </div>
@@ -17,9 +17,9 @@ const About: React.FC = () => {
 
       <div className="flex-grow container mx-auto">
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-          <div className="flex flex-col flex-none w-full pt-5 pb-5 pl-20 pr-20 md:auto">
-            <div className="bg-white rounded-lg shadow-lg p-8 flex-grow">
-              <div className="">
+          <div className="flex flex-col flex-none w-full pt-0 pb-5 md:pt-0 pr-20 pl-20">
+            <div className="rounded-lg p-8 flex-grow">
+              <div>
                 <p className="text-base text-gray-700 mb-4">{messages.lorem.p1}</p>
                 <p className="text-base text-gray-700 mb-4">{messages.lorem.p2}</p>
                 <p className="text-base text-gray-700 mb-4">{messages.lorem.p3}</p>
