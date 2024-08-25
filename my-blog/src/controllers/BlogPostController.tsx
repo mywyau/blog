915a8dbbe7a888b83dabd8b2185f524f73739224
mywyau <47248104+mywyau@users.noreply.github.com>
2024-08-25 14:40:45 +0100

    fix route

--- my-blog/src/controllers/BlogPostController.tsx
+++ my-blog/src/controllers/BlogPostController.tsx
@@ -1,11 +1,6 @@
-import { none, Option, some } from 'fp-ts/Option';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import BlogPostConnector from '../connectors/BlogPostConnector';
-import { DeleteResponseBody } from '../models/DeleteResponseBody';
-import { PostData } from '../models/PostData';
 import BlogPostPage from '../views/blog/BlogPostPage';
-import UseBlogPost from './hooks/UseBlogPost';
+import UseBlogPost from '../controllers/hooks/UseBlogPost';
+
 
 
 const BlogPostController: React.FC = () => {
