45cb5ffbbb7c866f4f82b845539a8fd613866269
mywyau <47248104+mywyau@users.noreply.github.com>
2024-09-04 21:59:59 +0100

    improve app quality

--- my-blog/src/views/pages/AboutPage.tsx
+++ my-blog/src/views/pages/AboutPage.tsx
@@ -3,21 +3,22 @@ import { messages } from '../../messages/about';
 import NavbarPages from '../../models/ADTs/NavbarPages';
 import Copyright from '../components/Copyright';
 import Navbar from '../components/navigation_bar/NavBar';
+import Spacer from '../components/Spacer';
 
 const AboutPage: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen font-nunito bg-gray-100">
       <Navbar page={NavbarPages.About} />
-      {/* <H1 id={"about"} message={messages.about.title} className={""} /> */}
-        <div className="flex flex-grow container mx-auto pt-28 flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-          <div className="flex flex-col flex-none w-full pt-0 pb-5 md:pt-0 pr-20 pl-20">
-            <div className="rounded-lg p-8">
-              <div>
-                <p className="text-base text-gray-700 mb-4">{messages.lorem.p1}</p>
-                <p className="text-base text-gray-700 mb-4">{messages.lorem.p2}</p>
-                <p className="text-base text-gray-700 mb-4">{messages.lorem.p3}</p>
-                <p className="text-base text-gray-700 mb-4">{messages.lorem.p4}</p>
-                <p className="text-base text-gray-700 mb-4">{messages.lorem.p5}</p>
+      <Spacer size="p-10"/>
+      <div className="flex flex-grow container mx-auto pt-28 flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+        <div className="flex flex-col flex-none w-full md:max-w-4xl lg:max-w-5xl pt-0 pb-5 md:pt-0 pr-8 pl-8 mx-auto">
+          <div className="rounded-lg p-8">
+            <div>
+              <p className="text-base text-gray-700 mb-4">{messages.lorem.p1}</p>
+              <p className="text-base text-gray-700 mb-4">{messages.lorem.p2}</p>
+              <p className="text-base text-gray-700 mb-4">{messages.lorem.p3}</p>
+              <p className="text-base text-gray-700 mb-4">{messages.lorem.p4}</p>
+              <p className="text-base text-gray-700 mb-4">{messages.lorem.p5}</p>
             </div>
           </div>
         </div>
