512bf59c1252113b9d66650335ddd463fa3214c6
mywyau <47248104+mywyau@users.noreply.github.com>
2024-09-07 18:32:38 +0100

    wip

--- my-blog/src/views/components/buttons/DeleteAllBlogPostsButton.tsx
+++ my-blog/src/views/components/buttons/DeleteAllBlogPostsButton.tsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import BlogPostConnector from "../../../connectors/BlogPostConnector";
-import UserTypes from '../../../models/ADTs/UserType';
+import { default as UserType, default as UserTypes } from '../../../models/ADTs/UserType';
 import { DeleteResponseBody } from '../../../models/DeleteResponseBody';
 import { PostData } from '../../../models/PostData';
 import AuthService from '../../../service/AuthService';
-import UserType from '../../../models/ADTs/UserType';
 
 const DeleteAllBlogPostsButton: React.FC<{ posts: PostData[]; setPosts: React.Dispatch<React.SetStateAction<PostData[]>> }> =
     ({ posts, setPosts }) => {
 
         const [deleteResponseBody, setDeleteResponseBody] = useState<DeleteResponseBody | null>(null);
         const [loading, setLoading] = useState<boolean>(false);
+        
         const [userRole, setUserRole] = useState<UserType | null>(null); // State to track user role
 
         // Fetch user role when component mounts
@@ -95,7 +95,7 @@ const DeleteAllBlogPostsButton: React.FC<{ posts: PostData[]; setPosts: React.Di
         } else if (userRole === UserTypes.Viewer) {
             return <></>; // Render message for non-admin users
         } else {
-            return <p>Trying to determine user type...</p>; // Show loading if user role is not yet determined
+            return <></>; // Render message for when not logged in at all
         }
     };
 
--- my-blog/src/views/pages/LandingPage.tsx
+++ my-blog/src/views/pages/LandingPage.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { default as UserType, default as UserTypes } from '../../models/ADTs/UserType';
 import { PostData } from '../../models/PostData';
+import AuthService from '../../service/AuthService';
 import BlogList from '../blog/BlogList';
 import DeleteAllBlogPostsButton from '../components/buttons/DeleteAllBlogPostsButton';
 import Copyright from '../components/Copyright';
 import Navbar from '../components/navigation_bar/NavBar';
 import Pagination from '../components/Pagination';
 import SearchBar from '../components/SearchBar';
-import { ToastContainer } from 'react-toastify';
 
 interface LandingPageProps {
   posts: PostData[];
@@ -14,7 +16,6 @@ interface LandingPageProps {
 }
 
 const LandingPage: React.FC<LandingPageProps> = ({ posts, errorMessage }) => {
-
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 5;
 
@@ -30,13 +31,52 @@ const LandingPage: React.FC<LandingPageProps> = ({ posts, errorMessage }) => {
 
   const [post, setPost] = useState<PostData[]>(posts);
 
+  const [userRole, setUserRole] = useState<UserType | null>(null); // State to track user role
+  // const [loadingUserRole, setLoadingUserRole] = useState<boolean>(true); // Track loading state
+
+  // Fetch user role with a slight delay for smoother transitions
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      try {
+        const role = await AuthService.getRole();
+        setUserRole(role);
+
+      } catch (error) {
+        console.error('Failed to fetch user role:', error);
+        setUserRole(null);
+      }
+    };
+
+    fetchUserRole();
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  function renderLoggedInContent(): JSX.Element {
+    
+    if (userRole === UserTypes.Admin) {
+      return (
+        <div>
+          <p className="text-lg text-purple-400 text-right">Logged in as Admin</p>
+        </div>
+      );
+    } else if (userRole === UserTypes.Viewer) {
+      return (
+        <div>
+          <p className="text-lg text-purple-400 text-right">Logged in as Viewer</p>
+        </div>
+      );
+    } else {
+      return (
+        <></>
+      );
+    }
+  }
+
   return (
     <div className="font-nunito min-h-screen bg-gray-100">
       <header className="">
         <Navbar />
       </header>
       <main className="container mx-auto p-10">
-
         {/* Use the SearchBar component */}
         <div className="w-full md:w-2/3 lg:w-1/3 mx-auto">
           <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
@@ -44,19 +84,19 @@ const LandingPage: React.FC<LandingPageProps> = ({ posts, errorMessage }) => {
 
         <div className="flex flex-grow container mx-auto font-nunito min-h-screen bg-gray-100">
           <div className="w-full md:w-3/4 lg:w-2/3 mx-auto">
-
             {currentPosts.length === 0 ? (
               <p className="text-center text-gray-500">No blog posts found.</p>
             ) : (
               <BlogList posts={currentPosts} />
             )}
 
+            {renderLoggedInContent()}
+
             <Pagination
               postsPerPage={postsPerPage}
               totalPosts={filteredPosts.length}
               paginate={(pageNumber) => setCurrentPage(pageNumber)}
             />
-
           </div>
         </div>
 
@@ -65,7 +105,6 @@ const LandingPage: React.FC<LandingPageProps> = ({ posts, errorMessage }) => {
             <DeleteAllBlogPostsButton posts={post} setPosts={setPost} />
           </div>
         </div>
-
       </main>
       <footer className="mt-auto">
         <Copyright />
